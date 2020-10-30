6edc83aeb56abeb74f50a58dcc600ba56f6f6050
Nissen99 <mikkelnj411@gmail.com>
2020-10-30 22:30:55 +0100

    14_02

---
+++ Exercise14_02/src/Ball.java
@@ -0,0 +1,14 @@
+public abstract class Ball extends Toy
+{
+  private int diameter;
+
+  public Ball(int suitableAge, int diameter)
+  {
+    super(suitableAge);
+    this.diameter = diameter;
+  }
+
+  public int getDiameter(){
+    return diameter;
+  }
+}
---
+++ Exercise14_02/src/Handball.java
@@ -0,0 +1,11 @@
+public class Handball extends Ball
+{
+  public Handball(int suitableAge, int diameter)
+  {
+    super(suitableAge, diameter);
+  }
+
+  public String play(){
+    return "Throw";
+  }
+}
