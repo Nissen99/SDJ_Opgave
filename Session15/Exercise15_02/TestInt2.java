80f72f3e1582a0b30c9af10982b9cfb0a134c9df
Mikkel <Mikkelnj411@gmail.com>
2020-11-02 16:19:44 +0100

    Session 15

--- Session15/Exercise15_02/TestInt2.java
+++ Session15/Exercise15_02/TestInt2.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class TestInt2
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello");
+
+    ArrayList<Integer> list = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    do
+    {
+
+        System.out.println("Please input an integer");
+        list.add(scanner.nextInt());
+
+
+
+
+    } while (true);
+
+
+
   }
 }
