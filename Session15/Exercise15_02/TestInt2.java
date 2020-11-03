076742c075cb5412a75745e095642131cef4a83d
Nissen99 <mikkelnj411@gmail.com>
2020-11-03 09:55:27 +0100

    Test2

--- Session15/Exercise15_02/TestInt2.java
+++ Session15/Exercise15_02/TestInt2.java
@@ -12,16 +12,20 @@ public class TestInt2
 
     do
     {
-
-        System.out.println("Please input an integer");
-        list.add(scanner.nextInt());
-
-
-
-
+try
+{
+  System.out.println("Please input an integer");
+  list.add(scanner.nextInt());
+} catch (InputMismatchException e){
+  if (scanner.nextLine().equals("end")){
+    break;
+  }
+}
     } while (true);
 
-
+    for (int i = 0; i < list.size(); i++){
+      System.out.println(list.get(i));
+    }
 
   }
 }
